28042d7977185e2342b34d05c9b1abaaf044a9ef
NathanKoleman <123844489+NathanKoleman@users.noreply.github.com>
2025-10-09 02:29:27 +0200

    added task1 code

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 # This is where the entry point of your solution should be
+import os, json
 #Task 0.1
 
 #Task 1
+
+def list_artists():
+    print("Artists found in the database:")
+    for artist_file in os.listdir("dataset/artists"):
+        with open("dataset/artists/" + artist_file, "r", encoding="utf-8") as current_file:
+            info = json.load(current_file)
+        print(f"- {info["name"]}")
 
 #Task 2
 
